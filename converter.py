3ff99ee7217ede926c62af741c75eb1040e9705b
Streetmentioner <streetms@student.21-school.ru>
2022-08-20 01:24:11 +0700

    photo converter

---
+++ converter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from PIL import Image
+import glob
+import argparse
+import os
+def create_parser():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("files",nargs='+')
+	parser.add_argument("--type",default='png')
+	parser.add_argument("--rm",default=False)
+	return parser
+
+def convert(file,args):
+	im = Image.open(file)
+	rgb_im = im.convert('RGB')
+	rgb_im.save(file.replace(file.split(".")[1], args.type), quality=95)
+	if (args.rm):
+		os.remove(file)
+		
+if __name__ == '__main__':
+	parser = create_parser()
+	args = parser.parse_args()
+	for file in args.files:
+		if '*' in file:
+			for file in glob.glob(file):
+				convert(file,args)
+		else:
+			convert(file,args)
+
